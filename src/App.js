d0f96fc776a74875168c3e3b1ae152d09be4f5bc
MichiMar <michimar.alexis@gmail.com>
2019-10-10 21:21:51 -0600

    updated my todo-list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import TodoItem from "./TodoItems";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      todos: [],
+      todo: ""
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:5000/todos")
+      .then(response => response.json())
+      .then(data => this.setState({ todos: data }));
+  }
+
+  renderTodos = () => {
+    return this.state.todos.map(todo => {
+      return (
+        <TodoItem
+          key={todo.id}
+          title={todo.title}
+          done={todo.done}
+          id={todo.id}
+          delete={this.deleteTodo}
+        />
+      );
+    });
+  };
+
+  handleChange = event => {
+    this.setState({ todo: event.target.value });
+  };
+
+  addTodo = event => {
+    event.preventDefault();
+    fetch("http://localhost:5000/todo", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({
+        title: this.state.todo,
+        done: false
+      })
+    })
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          todos: [...this.state.todos, data],
+          todo: ""
+        })
+      );
+  };
+
+  deleteTodo = id => {
+    fetch(`http://localhost:5000/todo/${id}`, {
+      method: "DELETE"
+    }).then(
+      this.setState({
+        todos: this.state.todos.filter(todo => todo.id !== id)
+      })
+    );
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Todo List</h1>
+        <form onSubmit={this.addTodo}>
+          <input
+            type="text"
+            placeholder="Add ToDo"
+            value={this.state.todo}
+            onChange={this.handleChange}
+          />
+          <button type="submit">Add</button>
+        </form>
+        {this.renderTodos()}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/TodoItems.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+class TodoItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      done: props.done
+    };
+  }
+
+  toggleDone = () => {
+    fetch(`http://localhost:5000/todo/${this.props.id}`, {
+      method: "PUT",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({
+        title: this.props.title,
+        done: !this.state.done
+      })
+      // @ts-ignore
+    }).then(this.setState({ done: !this.state.done }));
+  };
+
+  render() {
+    return (
+      <div className="todo-item">
+        <button onClick={() => this.props.delete(this.props.id)}>X</button>
+        <input
+          type="checkbox"
+          onChange={this.toggleDone}
+          defaultChecked={this.state.done}
+        />
+        <p className={this.state.done && "done"}>{this.props.title}</p>
+      </div>
+    );
+  }
+}
+
+export default TodoItem;
+
+// && logic operator, si el primero es true, aplica done, si no, no aplica nada, es nulo automatico
+/// || or, se anota en seguida el siguiente
